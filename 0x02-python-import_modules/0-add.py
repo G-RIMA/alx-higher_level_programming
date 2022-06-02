9fc24357231aac8b446ef1bccfe6310a036a96d8
gamal <gassuntamal2@gmail.com>
2022-06-02 04:08:29 -0700

    fix error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-   """ Print values a and b """
-   
-    from add_0 import add
-    #calling functions
-    
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+   from add_0 import add
+   #calling functions
+   a = 1
+   b = 2
+   print("{} + {} = {}".format(a, b, add(a, b)))
