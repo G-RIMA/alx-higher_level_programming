c41dbfab2216c1b5166fbf362a6be3b56189e953
gamal <gassuntamal2@gmail.com>
2022-06-03 23:39:24 -0700

    print element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# 1-element_at.py
+
+
 def element_at(my_list, idx):
     """ extract element from a list"""
     if idx < 0 or idx > len(my_list):
         return
-    else:
-        return my_list[idx]
+    return my_list[idx]
