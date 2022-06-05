4207be1ddc2a13a4536e6ce5cd2ce20551c5a6a2
gamal <gassuntamal2@gmail.com>
2022-06-05 09:56:52 -0700

    Remove c and C in a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+    """remove all the c and C characters in a string"""
+    x = [i for i in my_string if i != 'c' and i != 'C']
+    return ("".join(x))
