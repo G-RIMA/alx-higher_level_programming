9ffb4f05b6dc821cd6148be73f9dfe47e716e156
G_RIMA <gikandimaria178@gmail.com>
2022-09-06 14:33:22 +0300

    added a script that prints multiple lines

---
+++ 0x12-javascript-warm_up/ 1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myLine = 'C is fun';
+const myLine_1 = 'Python is Cool';
+const myLine_3 = 'Javascript is amazing';
+
+console.log(myLine)
+console.log(myLine_1)
+console.log(my_line_3)
