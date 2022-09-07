50e180817f670e73c058594c74796339ec2da5db
G_RIMA <gikandimaria178@gmail.com>
2022-09-07 13:49:25 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 // prints the factorial
 
 function factorial (n) {
-    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
 console.log(factorial(Number(process.argv[2])));
