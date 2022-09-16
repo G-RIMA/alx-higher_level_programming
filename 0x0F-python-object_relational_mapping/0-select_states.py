0c750220491295b59770c77706d52b9e1ac9a1ac
gamal <gassuntamal2@gmail.com>
2022-09-16 00:00:01 -0700

    fixed errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor= db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetall()
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
