522f6b2be41346f8a3a8353990dcae9de4a819b7
g-rima <gikandimaria178@gmail.com>
2022-12-16 17:43:21 +0300

    starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
+/** Script that prints the title of a Star War movie */
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const arg = process.argv[2];
+const url = `http://swapi.co/api/films/${arg}`;
+
+request.get(url, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(JSON.parse(body).title);
 });
