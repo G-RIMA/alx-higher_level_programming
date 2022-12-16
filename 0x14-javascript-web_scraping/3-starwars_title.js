e365ce4eb539931a70379d787adcc84e0b7adab5
g-rima <gikandimaria178@gmail.com>
2022-12-16 18:12:52 +0300

    fixed some errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,8 @@ const arg = process.argv[2];
 const url = `http://swapi.co/api/films/${arg}`;
 
 request.get(url, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(JSON.parse(body).title);
+  if (err) {
+    return console.log(err);
+  }
+  console.log(JSON.parse(body).title);
 });
